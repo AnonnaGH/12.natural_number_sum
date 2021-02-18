2be2edc7f638f0ad5a608aff1253416db590e5a2
“Anonna_Ferdaus” <anonnatahsin132@gmail.com>
2021-02-18 13:11:12 +0600

    First commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func main() {
+        m :=1000
+        sum:=0
+        for i:=0 ; i<m ; i++ {
+        if((i%3==0) || (i%5==0)){
+        
+         sum=sum+i
+
+}
+
+}      
+
+	fmt.Println(sum)
+}
